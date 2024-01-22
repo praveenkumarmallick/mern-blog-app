4232a61cb71ef2e7ec962ee3e122e9b3f5b53327
Praveen Kumar Mallick <140398553+praveenkumarmallick@users.noreply.github.com>
2024-01-22 19:36:59 +0530

    Create Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/Home"));
+const SignUp = lazy(() => import("./pages/SignUp"));
+const SignIn = lazy(() => import("./pages/SignIn"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Projects = lazy(() => import("./pages/Projects"));
 
 const App = () => {
   return (
-    <div className='text-red-500 bg-black px-3 py-1.5 text-3xl'>
-      App
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <SignUp />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <SignIn />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Dashboard />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/projects"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Projects />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
