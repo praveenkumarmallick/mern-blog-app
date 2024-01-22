6e255c8e360b1ca05a8cdd850f344158bee1f380
Praveen Kumar Mallick <140398553+praveenkumarmallick@users.noreply.github.com>
2024-01-22 21:10:40 +0530

    Creating test api

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ mongoose
   });
 
 const app = express();
+
+app.use("/api/user", userRoutes);
 
 app.listen(3000, () => {
   console.log(`Server is running on http://localhost:3000`);
