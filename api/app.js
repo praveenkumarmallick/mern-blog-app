2860bb90abb52b2834fc6e71f05bcca00b8d3406
Praveen Kumar Mallick <140398553+praveenkumarmallick@users.noreply.github.com>
2024-01-22 20:47:00 +0530

    Connecting to the Mongo DB

--- api/app.js
+++ api/app.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Database Connection
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Database Connected Successfully...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
-
-
 
 app.listen(3000, () => {
   console.log(`Server is running on http://localhost:3000`);
